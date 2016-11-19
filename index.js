fef623a824875e1c7cc9cf4084e7d5bf77e80b86
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2016-11-19 22:45:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function doToElementsInArray(array, callback)
+{
+  array.forEach(callback)
+}
+function changeCompletely(element, index, array)
+{
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
